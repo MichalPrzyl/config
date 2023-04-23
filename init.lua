d3586bf22d1d3befa4df3ef6da01fa2fff87b3c2
michal <michal@debian.myguest.virtualbox.org>
2023-04-23 03:44:56 +0200

    added github keymapping

--- init.lua
+++ init.lua
@@ -2,5 +2,8 @@
 
 vim.opt.relativenumber = true
 vim.keymap.set('n', '<leader>xd', "<cmd>:Neotree<cr>")
----aded
---new comment added
+vim.keymap.set('n', '<leader>git', "<cmd>:Git<cr>")
+vim.keymap.set('n', '<leader>com', "<cmd>:Git commit<cr>")
+vim.keymap.set('n', '<leader>push', "<cmd>:Git push<cr>")
+
+

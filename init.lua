2a0c8d068a999f7c08592c0a840c2b09f1a621f9
michalPrzyl <michal.przyl@gmail.com>
2023-04-23 18:59:23 +0200

    added structure, changed keymap, tabs
    - tab width for python programming
    - structure with comments
    - changed Neotree keymap from 'xd' to 'F6'

--- init.lua
+++ init.lua
@@ -1,9 +1,21 @@
 --  MP options
 
+
+local set = vim.opt -- set options
+
+
+-- relativenumber
 vim.opt.relativenumber = true
-vim.keymap.set('n', '<leader>xd', "<cmd>:Neotree<cr>")
+
+-- NeoTree
+vim.keymap.set({ 'n', 'v' }, '<F6>', ':Neotree <CR>')
+
+-- git commands with leader key
 vim.keymap.set('n', '<leader>git', "<cmd>:Git<cr>")
 vim.keymap.set('n', '<leader>com', "<cmd>:Git commit<cr>")
 vim.keymap.set('n', '<leader>push', "<cmd>:Git push<cr>")
 
-
+-- tabs
+set.tabstop = 2
+set.softtabstop = 2
+set.shiftwidth = 2

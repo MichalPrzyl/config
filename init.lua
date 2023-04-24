9350fcf48dca7aeabff73ad81f8b3bc2b5fb1a76
michalPrzyl <michal.przyl@gmail.com>
2023-04-25 01:55:22 +0200

    changes in `init.lua` file
    - added advanced neo-tree functionality
    - added neoscroll plugin
    In future i am planning upload plugins into this git repo

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+
 --  MP options
 
 local set = vim.opt -- set options
@@ -5,8 +6,10 @@ local set = vim.opt -- set options
 -- relativenumber
 vim.opt.relativenumber = true
 
--- NeoTree
-vim.keymap.set({ 'n', 'v' }, '<F6>', ':Neotree <CR>')
+-- Neotree and floating Neotree
+vim.keymap.set({ 'n', 'v' }, '<M-1>',  require('neo-tree').focus)
+vim.keymap.set({ 'n', 'v' }, '<M-2>',  require('neo-tree').close_all)
+vim.keymap.set({ 'n', 'v' }, '<M-3>',  require('neo-tree').float)
 
 -- git commands with leader key
 vim.keymap.set('n', '<leader>git', "<cmd>:Git<cr>")
@@ -27,5 +30,9 @@ enable_diagnostics = function()
   vim.diagnostic.enable()
 end
 
-vim.keymap.set({ 'n', 'v' }, '<F2>', disable_diagnostics)
-vim.keymap.set({ 'n', 'v' }, '<F3>', enable_diagnostics)
+vim.keymap.set({ 'n', 'v' }, '<F27>', enable_diagnostics) -- ctrl + <F3>
+vim.keymap.set({ 'n', 'v' }, '<F28>', disable_diagnostics) -- ctrl + <F4>
+
+-- toggle hlsearch
+vim.keymap.set({ 'n', 'v' }, '<F25>', "<cmd>:set hlsearch<cr>") -- ctrl + <F1>
+vim.keymap.set({ 'n', 'v' }, '<F26>', "<cmd>:set nohlsearch<cr>") -- ctrl + <F2>
